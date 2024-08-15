f49c8600b8c11fbe467ba54de1876501bc7855c1
SrjAdhikari <surajadhikari055@gmail.com>
2024-08-15 13:01:24 +0900

    Set up App component structure and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import {
+	Blog,
+	Features,
+	Footer,
+	Header,
+	Posibility,
+	WhatGPT3,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
 import "./App.css";
 
 function App() {
-	return <></>;
+	return (
+		<div className="App">
+			<div className="gradient__bg">
+				<Navbar />
+				<Header />
+			</div>
+			<Brand />
+			<WhatGPT3 />
+			<Features />
+			<Posibility />
+			<CTA />
+			<Blog />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
